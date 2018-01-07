8bd7df3d8745ca9af795790dadc8e8193b151cde
Jessica Le <jessicaduongle@gmail.com>
2018-01-06 22:27:57 -0600

    lectures 1 and 2.22 with first assignment done

---
+++ basics-assignment-1-start/src/app/nested-alert/nested-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component( {
+    selector: 'nested-alert',
+    templateUrl: './nested-alert.component.html',
+    styleUrls: ['./nested-alert.component.css']
+})
+
+export class NestedAlertComponent {}
---
+++ basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'warning-alert',
+    templateUrl: './warning-alert.component.html',
+    styleUrls: ['./warning-alert.componet.css']
+})
+
+export class WarningAlertComponent {}
