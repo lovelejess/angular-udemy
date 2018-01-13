841ab88b50d60ee79d15e93bfcd70bb8a3597b7f
Jessica Le <jessicaduongle@gmail.com>
2018-01-12 22:48:58 -0600

    Cool new recipes

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('recipe name', 'description', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
-    new Recipe('recipe name', 'description', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+    new Recipe('Mara’s Tofu With Mixed Grains', 'With only a few steps and five main ingredients, this simple dish barely requires a recipe, but the results are simultaneously nourishing and deeply satisfying.', 'https://static01.nyt.com/images/2018/01/14/magazine/14mag-eat/14mag-14eat-t_CA0-articleLarge.jpg'),
+    new Recipe('Roasted Chicken Thighs With Winter Squash', 'Roasted chicken thighs can be the juicy, meaty center of many weeknight meals. Add delicata squash, quickly tossed in a maple syrup-butter glaze, along with slices of lemon and sage, and you have a something more unusual, an interplay of flavors that don’t generally meet on the same sheet pan. ', 'https://static01.nyt.com/images/2014/03/12/dining/12squash/12squash-superJumbo.jpg')
   ];
 
   constructor() { }
