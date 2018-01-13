995fea398e00598909c110edc59ab31f7a706f61
Jessica Le <jessicaduongle@gmail.com>
2018-01-12 22:40:20 -0600

    using ngFor to display all recipies

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('recipe name', 'description', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+    new Recipe('recipe name', 'description', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
   ];
 
   constructor() { }
