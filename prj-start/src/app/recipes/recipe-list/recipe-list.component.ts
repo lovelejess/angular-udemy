03948e598cc0467c81113f01444ac262126b10c3
Jessica Le <jessicaduongle@gmail.com>
2018-01-29 21:33:09 -0600

    emitting events when selecting the recipe item

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from '../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
+  public recipes: Recipe[] = [
     new Recipe('Mara’s Tofu With Mixed Grains', 'With only a few steps and five main ingredients, this simple dish barely requires a recipe, but the results are simultaneously nourishing and deeply satisfying.', 'https://static01.nyt.com/images/2018/01/14/magazine/14mag-eat/14mag-14eat-t_CA0-articleLarge.jpg'),
     new Recipe('Roasted Chicken Thighs With Winter Squash', 'Roasted chicken thighs can be the juicy, meaty center of many weeknight meals. Add delicata squash, quickly tossed in a maple syrup-butter glaze, along with slices of lemon and sage, and you have a something more unusual, an interplay of flavors that don’t generally meet on the same sheet pan. ', 'https://static01.nyt.com/images/2014/03/12/dining/12squash/12squash-superJumbo.jpg')
   ];
+
+  @Output() recipeSelectedFromListEvent = new EventEmitter<Recipe>();
+
+  public onRecipeSelectedFromList(recipe: Recipe) {
+    this.recipeSelectedFromListEvent.emit(recipe);
+    console.log('onRecipeSelectedFromList');
+    console.log(recipe);
+  }
 
   constructor() { }
 
--- prj-start/src/app/recipes/recipes.component.ts
+++ prj-start/src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  public selectedRecipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onRecipeSelected(recipe: Recipe) {
+    this.selectedRecipe = recipe;
+    console.log('onRecipeSelected');
+    console.log(recipe);
   }
 
 }
