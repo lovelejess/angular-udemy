7494cfea303df46daaadcff954a25e1c548009d8
Jessica Le <jessicaduongle@gmail.com>
2018-02-11 09:12:06 -0600

    Remove event emitting

--- services-assignment-start/src/app/app.component.ts
+++ services-assignment-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from './users.service';
 
 @Component({
@@ -6,24 +6,4 @@ import { UserService } from './users.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  activeUsers: string[] = [];
-  inactiveUsers: string[] = [];
-
-  constructor(private userService: UserService) {
-    this.activeUsers = this.userService.activeUsers;
-    this.inactiveUsers = this.userService.inactiveUsers;
-  }
-  
-  onSetToInactive(id: number) {
-    // this.inactiveUsers.push(this.activeUsers[id]);
-    // this.activeUsers.splice(id, 1);
-    this.userService.setUserToInactive(id);
-  }
-
-  onSetToActive(id: number) {
-    // this.activeUsers.push(this.inactiveUsers[id]);
-    // this.inactiveUsers.splice(id, 1);
-    this.userService.setUserToActive(id);
-  }
-}
+export class AppComponent {}
