4eddaf318c2d2eb04e15b69884d27f7140e2a85c
Jessica Le <jessicaduongle@gmail.com>
2018-02-26 18:51:29 -0600

    created recipe service

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,26 +1,23 @@
 import { Recipe } from '../recipe.model';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
+
 })
 export class RecipeListComponent implements OnInit {
-  public recipes: Recipe[] = [
-    new Recipe('Mara’s Tofu With Mixed Grains', 'With only a few steps and five main ingredients, this simple dish barely requires a recipe, but the results are simultaneously nourishing and deeply satisfying.', 'https://static01.nyt.com/images/2018/01/14/magazine/14mag-eat/14mag-14eat-t_CA0-articleLarge.jpg'),
-    new Recipe('Roasted Chicken Thighs With Winter Squash', 'Roasted chicken thighs can be the juicy, meaty center of many weeknight meals. Add delicata squash, quickly tossed in a maple syrup-butter glaze, along with slices of lemon and sage, and you have a something more unusual, an interplay of flavors that don’t generally meet on the same sheet pan. ', 'https://static01.nyt.com/images/2014/03/12/dining/12squash/12squash-superJumbo.jpg')
-  ];
-
   @Output() recipeSelectedFromListEvent = new EventEmitter<Recipe>();
-
+  recipes: Recipe[] = [];
   public onRecipeSelectedFromList(recipe: Recipe) {
     this.recipeSelectedFromListEvent.emit(recipe);
   }
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
-
 }
