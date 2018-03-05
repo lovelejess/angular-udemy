00b765852401efe8a66e5b78b30e2c9dd79dbe37
Jessica Le <jessicaduongle@gmail.com>
2018-03-04 20:45:48 -0600

    add page not found

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -13,9 +13,10 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Server } from 'selenium-webdriver/safari';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: 'users', component: UsersComponent, children: [
     {path: ':id/:name', component: UserComponent},
   ]},
@@ -23,6 +24,8 @@ const appRoutes: Routes = [
     {path: ':id', component: ServerComponent},
     {path: ':id/edit', component: EditServerComponent},
   ]},
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found' }
 ];
 
 
@@ -34,7 +37,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
