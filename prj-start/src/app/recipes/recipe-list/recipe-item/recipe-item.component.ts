7c69884decf81d81cef3611711130e8eb07ab778
Jessica Le <jessicaduongle@gmail.com>
2018-03-10 17:50:26 -0600

    Get recipe by ID and show the recipe details

--- prj-start/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,13 +9,6 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() public recipeElement: Recipe;
   
-  constructor(private recipeService: RecipeService) { }
-
   ngOnInit() {
   }
-
-  public onRecipeItemSelected() {
-    this.recipeService.recipeSelected.emit(this.recipeElement);
-  }
-
 }
--- prj-start/src/app/recipes/recipe.service.ts
+++ prj-start/src/app/recipes/recipe.service.ts
@@ -34,6 +34,10 @@ export class RecipeService {
         return this.recipes.slice();
     }
 
+    public getRecipeById(id: number) {
+        return this.recipes[id];
+    }
+
     public addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.shoppingListService.addIngredients(ingredients);
     }
