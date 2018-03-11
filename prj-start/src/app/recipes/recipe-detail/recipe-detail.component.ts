7b01c08132f45dcd610521544f32d36ee32c47d7
Jessica Le <jessicaduongle@gmail.com>
2018-03-10 18:33:27 -0600

    new recipe

--- prj-start/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-start/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Params } from '@angular/router';
@@ -9,7 +9,8 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe: Recipe;
+  public recipe: Recipe;
+  @Input() public id: number;
 
   constructor(private recipeService: RecipeService, private route: ActivatedRoute) { 
 
@@ -17,12 +18,8 @@ export class RecipeDetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      this.recipe = this.recipeService.getRecipeById(+params['id']);
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipeById(this.id);
     });
   }
-
-  onSelectedNavigation() {
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
-  }
-
 }
