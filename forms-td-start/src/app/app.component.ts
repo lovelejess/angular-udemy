0c20f400b347a70e3508d18fb6c8ed7c7027e39f
Jessica Le <jessicaduongle@gmail.com>
2018-04-23 08:20:17 -0500

    user object

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -8,10 +8,17 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('userNameForm') signupForm: NgForm;
+  public submitted: boolean = false;
   public defaultQuestion: String = "teacher";
   public questionAnswer: String = "";
   public pronouns: String[] = ["male", "female", "non-binary", "prefer not to say"];
-  
+  public user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    pronoun: '' 
+  };
   public suggestUserName() {
     const suggestedName = 'Superuser';
     this.signupForm.form.patchValue({
@@ -28,5 +35,11 @@ export class AppComponent {
 
   public onSubmit() {
     console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.username;
+    this.user.email = this.signupForm.value.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.pronoun = this.signupForm.value.pronoun;
   }
 }
